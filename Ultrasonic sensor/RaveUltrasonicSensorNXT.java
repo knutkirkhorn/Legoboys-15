29d3bf4b8cd64de1be6d0fae6eabdbc170f89d9a
B-E-T-A <BobTheArchmage@hotmail.com>
2015-09-11 14:41:56 +0200

    NXT Ultrasonic
    Vi har lagt inn støtte for NXT ultrasonic sensor. Du finner selve
    "modulen" i RaveUltrasonicSensorNXT, men Ultrasonictest har modulen
    includert. Har også fått RaveUltrasonicSensor til å funke

--- Ultrasonic sensor/RaveUltrasonicSensorNXT.java
+++ Ultrasonic sensor/RaveUltrasonicSensorNXT.java
@@ -1,24 +1,25 @@
+import lejos.hardware.sensor.NXTUltrasonicSensor;
 import lejos.hardware.BrickFinder;
-import lejos.hardware.sensor.EV3UltrasonicSensor;
 import lejos.hardware.Brick;
 import lejos.hardware.port.Port;
 import lejos.hardware.Button;
 import lejos.robotics.SampleProvider;
 
-public class RaveUltrasonicSensor
+// Returnerer avstanden til hindringer i meter. Maks avstand er 2.5, mer enn det blir infinity
+
+class RaveUltrasonicSensorNXT
 {
-	private EV3UltrasonicSensor sensor;
+	private NXTUltrasonicSensor sensor;
 	private SampleProvider sampler;
 	private float[] sample;
 
-	public RaveUltrasonicSensor(Port port)
+	public RaveUltrasonicSensorNXT(Port port)
 	{
-		sensor = new EV3UltrasonicSensor(port);
+		sensor = new NXTUltrasonicSensor(port);
 		sampler = sensor.getDistanceMode();
-		sample = new float[sampler.sampleSize()];
+		sample = new float[sampler.sampleSize()]; // LYD
 	}
 
-	// Returnerer avstanden til hindringer i meter. Maks avstand er 2.5, mer enn det blir infinity
 	public float getDistance()
 	{
 		sampler.fetchSample(sample, 0);
--- Ultrasonic sensor/Ultrasonictest.java
+++ Ultrasonic sensor/Ultrasonictest.java
@@ -4,19 +4,23 @@ import lejos.hardware.Brick;
 import lejos.hardware.port.Port;
 import lejos.hardware.Button;
 import lejos.robotics.SampleProvider;
+import lejos.hardware.sensor.NXTUltrasonicSensor;
 
 public class Ultrasonictest
 {
 	 public static void main (String[] args) throws Exception
 	 {
 		 Brick brick = BrickFinder.getDefault();
-		 Port s1 = brick.getPort("S1"); // ultrasonic sensor
+		 Port s4 = brick.getPort("S4"); // ultrasonic sensor
+		 Port s1 = brick.getPort("S1"); // NXT ultrasonic sensor
 
-		 RaveUltrasonicSensor ultra = new RaveUltrasonicSensor(s1);
+		 RaveUltrasonicSensorNXT ultraNXT = new RaveUltrasonicSensorNXT (s1);
+		 RaveUltrasonicSensor ultra = new RaveUltrasonicSensor(s4);
 
 		 while (!Button.ESCAPE.isDown())
 		 {
 		 		 System.out.println(ultra.getDistance());
+		 		 System.out.println("NXT" + ultraNXT.getDistance());
 		 }
 	 }
 }
@@ -30,6 +34,25 @@ class RaveUltrasonicSensor
 	public RaveUltrasonicSensor(Port port)
 	{
 		sensor = new EV3UltrasonicSensor(port);
+		sampler = sensor.getDistanceMode();
+		sample = new float[sampler.sampleSize()]; // LYD
+	}
+
+	public float getDistance()
+	{
+		sampler.fetchSample(sample, 0);
+		return sample[0];
+	}
+}
+class RaveUltrasonicSensorNXT
+{
+	private NXTUltrasonicSensor sensor;
+	private SampleProvider sampler;
+	private float[] sample;
+
+	public RaveUltrasonicSensorNXT(Port port)
+	{
+		sensor = new NXTUltrasonicSensor(port);
 		sampler = sensor.getDistanceMode();
 		sample = new float[sampler.sampleSize()]; // LYD
 	}

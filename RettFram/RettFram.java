bce02ba8a332caa08aebcfe4e861ecb5ddc9c8a3
Knutakir <knutkirk@hotmail.com>
2015-09-12 12:11:44 +0200

    Test
    Test for å kjøre rett fram. Fordi roboten vår fuska litt med kjøringen.

---
+++ RettFram/RettFram.java
@@ -0,0 +1,20 @@
+import lejos.hardware.sensor.EV3UltrasonicSensor;
+import lejos.hardware.BrickFinder;
+import lejos.hardware.Brick;
+import lejos.hardware.port.Port;
+import lejos.hardware.Button;
+import lejos.robotics.SampleProvider;
+import lejos.hardware.motor.*;
+import lejos.robotics.navigation.*;
+import java.io.File;
+import lejos.hardware.Sound;
+
+class RettFram
+{
+	 public static void main (String[] args) throws Exception
+	 {
+		 DifferentialPilot pilot = new DifferentialPilot(56f, 126f, Motor.A, Motor.B);
+		 pilot.setTravelSpeed(600);
+		 pilot.forward();
+	 }
+}

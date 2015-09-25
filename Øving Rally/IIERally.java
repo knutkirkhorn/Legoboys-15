c9c85c1a5d44b34c2c651b8f116a3cd0e671f588
Knutakir <knutkirk@hotmail.com>
2015-09-25 15:55:06 +0200

    Nyere versjon
    Fungerer ikkje helt riktig.

--- Øving Rally/IIERally.java
+++ Øving Rally/IIERally.java
@@ -24,18 +24,22 @@ import lejos.hardware.sensor.NXTSoundSensor;
 import lejos.robotics.navigation.*;
 import lejos.hardware.Button;
 import lejos.hardware.sensor.NXTLightSensor;
-
+import lejos.hardware.lcd.TextLCD;
 
 public class IIERally{
 	public static void main (String[] args)  throws Exception{
 
-		DifferentialPilot pilot = new DifferentialPilot(56f, 126f, Motor.A, Motor.B);
-		pilot.setTravelSpeed(300);
+
+
+		DifferentialPilot pilot = new DifferentialPilot(56f, 126f, Motor.D, Motor.A);
+		pilot.setTravelSpeed(150);
 
 
 		Brick brick = BrickFinder.getDefault();
 		Port s1 = brick.getPort("S1"); //
 		Port s4 = brick.getPort("S4"); //
+
+		TextLCD lcd = brick.getTextLCD();
 
 		NXTLightSensor lysSensor = new NXTLightSensor(s1); // NXT LYS
 		EV3ColorSensor fargeSensor = new EV3ColorSensor(s4); // EV3 LYS
@@ -55,33 +59,37 @@ public class IIERally{
 		//----------------------------------------------------------------------
 
 		Boolean move = true; // Sett til false hvis roboten ikke skal kjøre videre
-		System.out.println("start lokke");
+		//System.out.println("start lokke");
 
 		while (!Button.ESCAPE.isDown()){ // Kjører så lenge vi ikke trykker på exit-knappen
-			
 			// Kjør framover hvis vi ikke allerede gjør det
 			if(!pilot.isMoving() && move) {
 				pilot.forward();
 			}
 
 			lysSensor.fetchSample(lysVerdi, 0); //LYS
-			float lys = lysVerdi[0] * 100;
+			float lys = lysVerdi[0];
 
 			fargeSensor.fetchSample(colorVerdi, 0); //COLOR
 			float color = colorVerdi[0];
 
+			lcd.clear();
+			lcd.drawString("NXT " + lys, 0, 0);
+			lcd.drawString("EV3 " + color, 0, 1);
+
 			if (color < 10){
-				System.out.println("svart color " + color);
-				pilot.rotate(10); // VENSTRE
-			} else if (lys > 48){
-				System.out.println("svart lys " + lys);
-				pilot.rotate(-10); //HØYRE
+
+				pilot.rotate(-10); // VENSTRE
+			} else if (lys < 48){
+
+				//pilot.arcForward(-150);
+				pilot.rotate(10);
 			}
 			// Elsen under trengs ikke, da forward kjøres på starten uansett
 			// else {
 				// pilot.forward();
 			// }
-			
+
 		}//while
 		System.out.println("AVSLUTTET");
 	}//main

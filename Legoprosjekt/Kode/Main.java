22e98cb544bd949e98aea41d88dffd52d6cbd93a
Audun Arbo <audunwar@student.hist.no>
2015-10-29 12:33:50 +0100

    La til base for å bruke leJOS menykode.

--- Legoprosjekt/Kode/Main.java
+++ Legoprosjekt/Kode/Main.java
@@ -13,12 +13,19 @@ public class Main
 	// Contains alle the dispensers
 	private static Dispenser[] dispensers;
 	
+	// The menu component
+	private static GraphicMenu menu;
+	private static String[] menuTitles = new String[] { "Element1", "Element2", "Element3" };
+	private static String[] menuIcons = new String[] { Icons.TEST, Icons.TEST, Icons.TEST };
+	
 	//Entry point and main method of application
 	public static void main(String[] args)
 	{
 		brick = BrickFinder.getDefault();
 		belt = Motor.A; // Conveyor belt connected to port A
 		dispensers = new Dispenser[1]; // 1 dispenser for testing
+		
+		menu = new GraphicMenu(menuTitles, menuIcons, 1, "Drinkmikser", 0);
 		
 		InitializeDispensers();
 	}
